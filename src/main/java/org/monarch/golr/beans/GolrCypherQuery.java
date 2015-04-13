3bd89b63ebfe87b2bb43f66a5d26e4ccf1f0be01
Christopher Condit <condit@sdsc.edu>
2015-04-13 10:24:00 -0700

    Adds cypher query bean

---
+++ src/main/java/org/monarch/golr/beans/GolrCypherQuery.java
@@ -0,0 +1,29 @@
+package org.monarch.golr.beans;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
+import edu.sdsc.scigraph.neo4j.DirectedRelationshipType;
+
+public class GolrCypherQuery {
+
+  private String query;
+  private Multimap<String, DirectedRelationshipType> types = HashMultimap.create();
+  private Map<String, String> projection = new HashMap<>();
+
+  public String getQuery() {
+    return query;
+  }
+
+  public Multimap<String, DirectedRelationshipType> getTypes() {
+    return types;
+  }
+
+  public Map<String, String> getProjection() {
+    return projection;
+  }
+
+}
---
+++ src/test/java/org/monarch/golr/YamlSerializationTest.java
@@ -0,0 +1,46 @@
+package org.monarch.golr;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
+
+import java.io.IOException;
+
+import org.hamcrest.collection.IsMapContaining;
+import org.junit.Before;
+import org.junit.Test;
+import org.monarch.golr.beans.GolrCypherQuery;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+
+import edu.sdsc.scigraph.neo4j.DirectedRelationshipType;
+
+public class YamlSerializationTest {
+
+  ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+
+  @Before
+  public void setup() {
+    mapper.registerModule(new GuavaModule());
+  }
+
+  @Test
+  public void test() throws JsonParseException, JsonMappingException, IOException {
+    GolrCypherQuery query = mapper.readValue(
+        "query: cypher query\n"
+        + "projection:\n"
+        + "  foo: bar\n"
+        + "types:\n"
+        + "  foo:\n"
+        + "    - type: partOf\n"
+        + "      direction: OUTGOING\n", GolrCypherQuery.class);
+    assertThat(query.getQuery(), is("cypher query"));
+    assertThat(query.getProjection(), IsMapContaining.<String, String>hasEntry("foo", "bar"));
+    assertThat(query.getTypes().get("foo"), contains(new DirectedRelationshipType("partOf", "OUTGOING")));
+  }
+
+}
